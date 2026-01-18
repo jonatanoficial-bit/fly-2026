ce4159f827ac241ed26c0959856d9401e05fa70e
jonatanoficial-bit <jonatanoficial@gmail.com>
2026-01-18 14:47:08 -0300

    Criar js/firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,13 @@
+// js/firebase-config.js
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyD3bQZBTCcpb5ERx4XyfC7DLwrrF8G-YuQ",
+  authDomain: "fly-2026.firebaseapp.com",
+  projectId: "fly-2026",
+  storageBucket: "fly-2026.firebasestorage.app",
+  messagingSenderId: "1070874157285",
+  appId: "1:1070874157285:web:7f9ca52e17406ff0a2740c",
+  measurementId: "G-BXHQSTNH1F"
+};
+
+// UID ADMIN (o seu)
+window.ADMIN_UID = "Nf4mGSSXoVhN8Y8lHsP31khYikL2";
