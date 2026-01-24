bf8c33a02ce4c5b8c37252f488837d4bfa35e290
jonatanoficial-bit <jonatanoficial@gmail.com>
2026-01-24 16:13:49 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
-self.addEventListener('install',e=>{e.waitUntil(caches.open('flysim').then(c=>c.addAll(['./','index.html'])))});
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('flysim-cache-v1').then((cache) => cache.addAll([
+      './',
+      './index.html',
+      './manifest.json'
+    ]))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+});
